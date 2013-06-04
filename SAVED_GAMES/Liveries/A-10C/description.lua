241cfc348c9f85979f7f2eb279f79495fc228b88
bob <dariboucafix@daribouca.net>
2013-06-04 19:19:44 +0200

    initial import

---
+++ SAVED_GAMES/Liveries/A-10C/description.lua
@@ -0,0 +1,21 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"A-10C_tdc-a",true};
+	{"A-10C_PAINT_1-b", 0 ,"A-10C_tdc-b",true};
+	{"A-10C_PAINT_1-c", 0 ,"A-10C_tdc-c",true};
+	{"A-10C_PAINT_1-d", 0 ,"A-10C_tdc-d",true};
+	{"A-10C_PAINT_1-e", 0 ,"A-10C_tdc-e",true};
+	{"A-10C_PAINT_1-f", 0 ,"A-10C_tdc-f",true};
+	{"A-10C_PAINT_1-g", 0 ,"A-10C_tdc-g",true};
+	{"A-10C_PAINT_1-h", 0 ,"A-10C_tdc-h",true};
+	{"A-10C_PAINT_1-i", 0 ,"A-10C_tdc-i",true};
+	{"A-10C_PAINT_1-j", 0 ,"A-10C_tdc-j",true};
+	{"A-10C_PAINT_1-k", 0 ,"A-10C_tdc-k",true};
+	{"A-10C_PAINT_1-L", 0 ,"A-10C_tdc-L",true};
+	{"A-10_Number", 0 ,"TactNumbers-USAF-Light_black",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"TactNumbers-USAF-Light_black",true};
+}
+countries = {"USA", "BEL", "CAN", "FRA"}
+-- updated 2012/03/24 [BOB]
+-- updated 2012/02/12 22:40 [BOB]
---
+++ install.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+__author__ = "bob"
+
+special = {
+    "append": "SAVED_GAMES/Config/autoexec.cfg"
+}
